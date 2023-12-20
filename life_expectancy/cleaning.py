05a5810a418e2fa03fb38855e10ef94b7f2855fd
goncalosantiago99 <goncalo.santiago.pereira@nos.pt>
2023-12-20 14:49:21 +0000

    assignment 2 - v2

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -6,13 +6,23 @@ def clean_data( df: pd.DataFrame, region: str) -> pd.DataFrame:
     """
     This function does a full cleanse regarding the dataframe
     originated by the TST file
+    Args:
+    df - tsv files as a pandas dataframe
+    region - the region we want to filter the dataframe by
+
+    Returns:
+    filtered_df - the cleaned dataframe
     """
 
-    splited_df = split_column(df, r"unit,sex,age,geo\time", ",")
+    # create list that identifies the group of columns which will
+    # caracterize each row
+    attribute_cols = ["unit", "sex", "age", "region"]
 
-    ordered_df = order_columns(splited_df)
+    splited_df = split_column(df, r"unit,sex,age,geo\time", ",", attribute_cols)
 
-    long_df = pivot_df(ordered_df)
+    ordered_df = order_columns(splited_df, attribute_cols)
+
+    long_df = pivot_df(ordered_df, attribute_cols)
 
     df_without_space = work_spaces(long_df, "value")
 
@@ -24,7 +34,7 @@ def clean_data( df: pd.DataFrame, region: str) -> pd.DataFrame:
 
     # 'no_nan_df' is filtered by the region received as input
     # on the 'clean_data' function
-    filtered_df = no_nan_df.loc[no_nan_df['region'] == region]
+    filtered_df = no_nan_df[no_nan_df['region'] == region]
 
     return filtered_df
 
@@ -32,31 +42,49 @@ def load_data(path_inp: str) -> pd.DataFrame:
     """
     This function loads the TSV file stored in the received
     path and loads it into a pandas dataframe
+    Args:
+    path_inp - path of the TSV file
+
+    Returns: the tsv file as a pandas dataframe
     """
 
     df = pd.read_csv(path_inp, sep='\t')
 
     return df
 
-def split_column(df: pd.DataFrame, name_column: str, delimeter: str) -> pd.DataFrame:
+def split_column(df: pd.DataFrame, name_column: str, delimeter: str, atb_col: list) -> pd.DataFrame:
     """
     This function splits the values in the specific column based on a delimeter and 
     expands them into new columns. It also drops the column that was splited.
+    Args:
+    df - the dataframe to be splited
+    name_column - the column to be splited
+    delimeter - the delimeter that will split the column
+    atb_col - the list of columns that will be created after the split
+
+    Returns: 
+    df - the dataframe now with the splited column
     """
 
-    df[["unit", "sex", "age", "region"]] = df[name_column].str.split(delimeter, expand=True)
+    df[atb_col] = df[name_column].str.split(delimeter, expand=True)
 
     df = df.drop(columns=[name_column])
 
     return df
 
-def order_columns(df: pd.DataFrame) -> pd.DataFrame:
+def order_columns(df: pd.DataFrame, attribute_cols: list) -> pd.DataFrame:
     """
     This function orders the columns in the dataframe
+    Args:
+    df - the dataframe to be ordered
+    attribute_cols - the list of columns that will be created after the split
+
+    Returns: 
+    df - the dataframe ordered
     """
 
     # create list that identifies the columns that shoud come first
-    init_col = ["unit", "sex", "age", "region"]
+    init_col = attribute_cols
 
     # reorder the datafrane columns based on the 'init_col' list
     new_column_order = init_col + [col for col in df.columns if col not in init_col]
@@ -64,14 +92,21 @@ def order_columns(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def pivot_df(df: pd.DataFrame) -> pd.DataFrame:
+def pivot_df(df: pd.DataFrame, attribute_cols: list) -> pd.DataFrame:
     """
     This function pivots the dataframe regarding the columns
     that represent a year
+    This function orders the columns in the dataframe
+    Args:
+    df - the dataframe to be pivoted
+    attribute_cols - the list of columns that will remain unpivoted
+
+    Returns: 
+    df - the dataframe pivoted
     """
 
     # create list that identifies the columns that shoud remain unpivoted
-    unpivot_col = ["unit", "sex", "age", "region"]
+    unpivot_col = attribute_cols
 
     # pivots the dataframe to long format
     long_df = pd.melt(df, id_vars = unpivot_col, var_name='year', value_name='value')
@@ -82,6 +117,12 @@ def work_spaces(df: pd.DataFrame, monitor_column: str) -> pd.DataFrame:
     """
     This function deletes the spaces in the column that is given
     as input
+    Args:
+    df - the dataframe to transformed
+    monitor_column - the column that will be transformed
+
+    Returns: 
+    df - the dataframe without spaces in the desired column
     """
 
     # remove leading and trailing spaces, and convert to numeric values
@@ -93,6 +134,13 @@ def delete_non_numeric(df: pd.DataFrame, monitor_column: str) -> pd.DataFrame:
     """
     This function deletes all the rows that aren't numerical regarding
     the column that is given as input
+    Args:
+    df - the dataframe to transformed
+    monitor_column - the column that will be transformed
+
+    Returns: 
+    df - the dataframe without rows that aren't numerical regarding a
+    specific column
     """
 
     # remove all chars that aren't numbers and '.'
@@ -104,6 +152,11 @@ def convert_to_float(value: str) -> float:
     """
     This function converts the input column to float and replaces 
     non-convertible values with NaN
+    Args:
+    value - the column to be converted to float
+
+    Returns: 
+    df - the converted column
     """
     try:
         return float(value)
@@ -113,6 +166,11 @@ def convert_to_float(value: str) -> float:
 def set_datatypes(df: pd.DataFrame) -> pd.DataFrame:
     """
     This function converts the datatypes to the desired ones
+    Args:
+    df - the dataframe to be transformed
+
+    Returns: 
+    df - the transformed dataframe
     """
 
     # define a dictionary specifying the data types
@@ -135,6 +193,9 @@ def set_datatypes(df: pd.DataFrame) -> pd.DataFrame:
 def save_data(df: pd.DataFrame, output_df_path: str) -> None:
     """
     This function saves the filtered dataframe to the data folder
+    Args:
+    df - the dataframe to be converted to CSV
+    output_df_path - the path where the CSV file will be created
     """
 
     df.to_csv(output_df_path, index=False)
