b320771713d8eaedc5458474f167851d3cfc248f
goncalosantiago99 <goncalo.santiago.pereira@nos.pt>
2023-12-30 13:10:58 +0000

    assignment 3 - v3

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -1,62 +1,20 @@
 """Pytest configuration file"""
-import pandas as pd
 import pytest
-from life_expectancy.cleaning import clean_data
-from . import FIXTURES_DIR#, OUTPUT_DIR
-
-def sample_with_pt(df: pd.DataFrame, sample_size: int) -> pd.DataFrame:
-    """
-    This function will get a df and extract a sample from it, with a size equal to
-    "sample_size". However, it assures there is at least one row with from the "PT" region.
-    Parameters:
-    df - Loaded data from the TSV
-    sample_size - Size of the sample 
-    
-    Return:
-    sample - Dataframe that represents the sample of the input dataframe
-    """
-    # create a sample from the input dataframe
-    sample = df.sample(sample_size)
-
-    # while there is not "PT" row we will keep sampling
-    while len(sample[sample[r'unit,sex,age,geo\time'].str.endswith('PT')]) == 0:
-        sample = df.sample(sample_size)
-
-    return sample
+import pandas as pd
+from . import FIXTURES_DIR
 
 @pytest.fixture(scope="session")
-def life_expectancy_input() -> pd.DataFrame:
-    """Fixture to return the path to the expected input of the cleaning script"""
-
-    # creates panda dataframe after a TSV file
-    df = pd.read_csv("./life_expectancy/data/eu_life_expectancy_raw.tsv",
-        sep='\t')
-
-    # sample the df
-    sample_df = sample_with_pt(df.copy(), 500)
-
-    # store the sampled df in a tsv file in a specific path
-    sample_df.to_csv("./life_expectancy/tests/fixtures/eu_life_expectancy_raw.tsv",
-        sep='\t',
-        index=False)
-
+def life_expectancy_input_path() -> pd.DataFrame:
+    """
+    Fixture to return the path to the sampled tsv file that
+    represents the eu_life_expectancy
+    """
     return FIXTURES_DIR / "eu_life_expectancy_raw.tsv"
 
 @pytest.fixture(scope="session")
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
 
-    # creates panda dataframe after a TSV file
-    df = pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv",
-        sep='\t')
-
-    # clean the sampled df
-    df_cleaned = clean_data(df, "PT")
-
-    # store the cleaned df in a tsv file in a specific path
-    df_cleaned.to_csv("./life_expectancy/tests/fixtures/pt_life_expectancy_expected.csv",
-        index=False)
-
-    df_cleaned = df_cleaned.reset_index(drop=True)
+    df_cleaned = pd.read_csv( FIXTURES_DIR / "pt_life_expectancy_expected.csv")
 
     return df_cleaned
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -5,13 +5,13 @@ import pandas as pd
 from life_expectancy.cleaning import load_data, clean_data, save_data
 from . import OUTPUT_DIR, FIXTURES_DIR
 
-def test_load(life_expectancy_input):
+def test_load(life_expectancy_input_path):
     """
     Loads the data from the tsv file to a pandas dataframe
     """
 
     # load the data from the tsv
-    df = load_data(life_expectancy_input)
+    df_loaded = load_data(life_expectancy_input_path)
 
     # load the dataframe that was just cleaned and created
     eu_life_expectancy_actual = pd.read_csv(
@@ -20,36 +20,37 @@ def test_load(life_expectancy_input):
     )
 
     pd.testing.assert_frame_equal(
-        df, eu_life_expectancy_actual
+        df_loaded, eu_life_expectancy_actual
     )
 
-def test_clean_data(life_expectancy_input, pt_life_expectancy_expected):
+def test_clean_data(life_expectancy_input_path, pt_life_expectancy_expected):
 
     #Loads the data from the tsv file to a pandas dataframe
-    df = load_data(life_expectancy_input)
+    df_loaded = load_data(life_expectancy_input_path)
 
     # the region to filter the dataframe by
     region = "PT"
 
     # clean the data
-    df_cleaned = clean_data(df, region)
+    df_cleaned = clean_data(df_loaded, region)
 
-    df_cleaned = df_cleaned.reset_index(drop=True)
+    #df_cleaned = df_cleaned.reset_index(drop=True)
+    #pt_life_expectancy_expected = pt_life_expectancy_expected.reset_index(drop=True)
 
     pd.testing.assert_frame_equal(
         df_cleaned, pt_life_expectancy_expected
     )
 
-def test_save(life_expectancy_input, pt_life_expectancy_expected):
+def test_save(life_expectancy_input_path, pt_life_expectancy_expected):
 
     #Loads the data from the tsv file to a pandas dataframe
-    df = load_data(life_expectancy_input)
+    df_loaded = load_data(life_expectancy_input_path)
 
     # the region to filter the dataframe by
     region = "PT"
 
     # cleans the loaded df
-    df_cleaned = clean_data(df, region)
+    df_cleaned = clean_data(df_loaded, region)
 
     # path where the cleaned df will be stored
     out_path: Path  = OUTPUT_DIR / "pt_life_expectancy.csv"
@@ -58,9 +59,7 @@ def test_save(life_expectancy_input, pt_life_expectancy_expected):
     save_data(df_cleaned, out_path)
 
     # load the dataframe that was just cleaned and created
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+    pt_life_expectancy_actual = pd.read_csv(out_path)
 
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
